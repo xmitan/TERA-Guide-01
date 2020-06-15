27fa7619985a1aeb4b95f9490ea97b3db8ed347c
海比丶天蓝 <149352394@qq.com>
2020-06-15 19:28:00 +0800

    1

---
+++ boss-Custom.js
@@ -0,0 +1,173 @@
+// Translation Edition (Custom Edit)
+module.exports = {
+	// Into Dungeon Tip-Msg
+	DungeonInfo : [
+		{zone: 9066, string: `Welcome to <font color="#56B4E9">Demon's Wheel</font>`},
+		{zone: 9059, string: `Welcome to <font color="#56B4E9">Forsaken Island</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9759, string: `Welcome to <font color="#56B4E9">Forsaken Island</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 9767, string: `Welcome to <font color="#56B4E9">Demokron Factory</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9067, string: `Welcome to <font color="#56B4E9">Demokron Factory</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 9770, string: `Welcome to <font color="#56B4E9">Ruinous Manor</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9970, string: `Welcome to <font color="#56B4E9">Ruinous Manor</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 9781, string: `Welcome to <font color="#56B4E9">Velik's Sanctuary</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9981, string: `Welcome to <font color="#56B4E9">Velik's Sanctuary</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 9735, string: `Welcome to <font color="#56B4E9">RK-9 Kennel</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9935, string: `Welcome to <font color="#56B4E9">RK-9 Kennel</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 9739, string: `Welcome to <font color="#56B4E9">Red Refuge</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9939, string: `Welcome to <font color="#56B4E9">Red Refuge</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 9720, string: `Welcome to <font color="#56B4E9">Antaroth's Abyss</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9920, string: `Welcome to <font color="#56B4E9">Antaroth's Abyss</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 3017, string: `Welcome to <font color="#56B4E9">Antaroth's Abyss</font> <font color="#FF0000"> 7-Plays</font>`},
+		{zone: 9783, string: `Welcome to <font color="#56B4E9">Dark Reach Citadel</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9983, string: `Welcome to <font color="#56B4E9">Dark Reach Citadel</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 3018, string: `Welcome to <font color="#56B4E9">Dark Reach Citadel</font> <font color="#FF0000"> 7-Plays</font>`},
+		{zone: 9782, string: `Welcome to <font color="#56B4E9">Grotto of Lost Souls</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9982, string: `Welcome to <font color="#56B4E9">Grotto of Lost Souls</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 3019, string: `Welcome to <font color="#56B4E9">Grotto of Lost Souls</font> <font color="#FF0000"> 7-Plays</font>`},
+		{zone: 9044, string: `Welcome to <font color="#56B4E9">Bahaar's Sanctum</font>`},
+		{zone: 3101, string: `Welcome to <font color="#56B4E9">Gossamer Vault</font> <font color="#E69F00">Normal</font>`},
+		{zone: 3201, string: `Welcome to <font color="#56B4E9">Gossamer Vault</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 3023, string: `Welcome to <font color="#56B4E9">Akalath Quarantine</font>`},
+		{zone: 3020, string: `Welcome to <font color="#56B4E9">Sea of Honor</font>`},
+		{zone: 3026, string: `Welcome to <font color="#56B4E9">Corrupted Skynest</font> <font color="#E69F00">Normal</font>`},
+		{zone: 3126, string: `Welcome to <font color="#56B4E9">Corrupted Skynest</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 3027, string: 'Welcome to <font color="#56B4E9">Forbidden Arena</font>'}
+	],
+	// Demon's Wheel
+	DW_BOSS_1 : [
+	],
+	DW_TipMsg1: [],
+	DW_BOSS_2 : [
+	],
+	DW_TipMsg2: [
+	],
+	// Forsaken Island
+	FI_BOSS_1 : [
+	],
+	FI_BOSS_2 : [
+	],
+	FI_BOSS_3 : [
+	],
+	FI_TipMsg : [],
+	// Demokron Factory
+	DF_BOSS_1 : [
+	],
+	DF_BOSS_2 : [
+	],
+	DF_BOSS_3 : [
+	],
+	// Ruinous Manor
+	RM_BOSS_1 : [
+	],
+	RM_BOSS_2 : [
+	],
+	RM_BOSS_3 : [
+	],
+	// Velik's Sanctuary
+	VS_BOSS_1 : [
+	],
+	VS_BOSS_2 : [
+	],
+	VS_BOSS_3 : [
+	],
+	VS_TipMsg : [],
+	// RK-9 Kennel
+	RK_BOSS_1 : [
+	],
+	RK_BOSS_2 : [
+	],
+	RK_BOSS_3 : [
+	],
+	RK_TipMsg : [],
+	// Red Refuge
+	RR_BOSS_1 : [
+	],
+	RR_BOSS_2 : [
+	],
+	RR_BOSS_3 : [
+	],
+	// Antaroth's Abyss
+	AA_BOSS_1 : [
+	],
+	AA_BOSS_2 : [
+	],
+	AA_BOSS_3 : [
+	],
+	// Dark Reach Citadel
+	DRC_BOSS_1 : [
+	],
+	DRC_BOSS_2 : [
+	],
+	DRC_BOSS_3 : [
+	],
+	DRC_TipMsg : [],
+	// Grotto of Lost Souls
+	GLS_BOSS_1 : [
+	],
+	GLS_BOSS_2 : [
+	],
+	GLS_BOSS_3 : [
+	],
+	// Bahaar's Sanctum
+	BS_BOSS : [
+	],
+	BS_TipMsg : [],
+	// Gossamer Vault
+	GV_BOSS_1 : [
+		{id: 1104, msg: 'Jump Stun'},
+		{id: 2104, msg: 'Jump Stun'},
+		{id: 1110, msg: 'Front Stun'},
+		{id: 2110, msg: 'Front Stun'},
+		{id: 1111, msg: 'Left Slash'},
+		{id: 1113, msg: 'Left Slash'},
+		{id: 2111, msg: 'Left Slash'},
+		{id: 2113, msg: 'Left Slash'},
+		{id: 1112, msg: 'Right Slash'},
+		{id: 1114, msg: 'Right Slash'},
+		{id: 2112, msg: 'Right Slash'},
+		{id: 2114, msg: 'Right Slash'},
+		{id: 1115, msg: 'Backslash'},
+		{id: 2115, msg: 'Backslash'},
+		{id: 1116, msg: 'Explosion (Shield)!!'},
+		{id: 2116, msg: 'Explosion (Shield)!!'},
+		{id: 3107, msg: 'Forward Wave'},
+		{id: 3115, msg: 'Spin'},
+		{id: 3116, msg: 'Circles + Spin'},
+		{id: 3119, msg: 'Debuff ', TIP: [" Get OUT", " Get IN"]},
+		{id: 3220, msg: 'Debuff ', TIP: [" Get IN", " Get OUT"]}
+	],
+	GV_BOSS_2 : [
+		{id: 164, msg: 'Hold (Bleed)'},
+		{id: 166, msg: 'Turn around'},
+		{id: 175, msg: 'Roar (iframe)!!'},
+		{id: 181, msg: 'Rock'},
+		// 214 Triple Rocks 181
+		{id: 182, msg: 'Stomp (Knockdown)'},
+		// 302 209 Stomp (Knockdown) 182 183 184
+		{id: 185, msg: 'Explosion (Shield)!!'},
+		{id: 202, msg: 'Back + Front Stab'},
+		{id: 207, msg: 'AimBack (Bleed)'},
+		{id: 212, msg: 'Bait (Bleed)'}
+	],
+	// Akalath Quarantine
+	AQ_BOSS_1 : [
+	],
+	AQ_BOSS_2 : [
+	],
+	// Sailing Instance / Sea of Honor
+	SI_BOSS_1 : [
+	],
+	SI_BOSS_2 : [
+	],
+	SI_BOSS_3 : [
+	],
+	SI_TipMsg : [],
+	// Corrupted Skynest
+	CK_BOSS : [
+	],
+	CK_TipMsg : [],
+	// Forbidden Arena
+	FA_BOSS : [
+	],
+	FA_TipMsg : []
+}
